a67203de1f2f0a512c72f14df5de78dfea3c1d31
Valeria Balducci <v_balducci@web.de>
2023-07-03 17:27:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// "@testing-library/jest-dom": "^5.16.2",
+    // "@testing-library/react": "^11.2.7",
+    // "@testing-library/user-event": "^12.8.3",
+    // "react": "^17.0.2",
+    // "react-dom": "^17.0.2",
+    //Once done, run npm install
+    // put in json
+    
