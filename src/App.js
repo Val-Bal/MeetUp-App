dbf1f7651eb2d641241fb5154565fc0aa1d4e1e5
Valeria Balducci <v_balducci@web.de>
2023-07-08 13:54:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
- }
+ return (
+   <div className="App">
+     <CitySearch />
+     <EventList />
+   </div>
+ );
+}
 
 export default App;
-
-// "@testing-library/jest-dom": "^5.16.2",
-    // "@testing-library/react": "^11.2.7",
-    // "@testing-library/user-event": "^12.8.3",
-    // "react": "^17.0.2",
-    // "react-dom": "^17.0.2",
-    //Once done, run npm install
-    // put in json
     
