abd1de47da7d98751a43f4dc289f92db91d13e79
Valeria Balducci <v_balducci@web.de>
2023-07-24 10:30:09 +0200

    Update EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -41,9 +41,43 @@ describe('show/hide event details', () => {
     expect(eventDetails).toBeDefined();
   });
 
-  test('User can collapse an event to hide details', async () => {
+  test('User can collapse an event to hide its details', async () => {
     await page.click('.event .details-btn');
-    const eventDetails = await page.$('.event .details');
+    const eventDetails = await page.$('.event .event__Details');
     expect(eventDetails).toBeNull();
   });
+
+});
+
+describe('filter events by city', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  });
+
+  afterAll(() => {
+      browser.close();
+  });
+
+  test('When user has not searched for a city, show upcoming events from all cities.', async () => {
+      const eventDetails = await page.$('.event .event-details');
+      expect(eventDetails).toBeDefined();
+  });
+
+  test('User should see a list of suggestions when they search for a city', async () => {
+      const CitySearch = await page.$('.suggestions li');
+      expect(CitySearch).toBeDefined()
+  });
+
+  test('User can select a city from the suggested list', async () => {
+      await page.reload();
+      await page.type('.city', 'Berlin', { delay: 100 });
+      await page.click('.suggestions li');
+      const selectCity = await page.$$eval('.event', (element) => element.length);
+      expect(selectCity).toBe(1);
+  })
 });
