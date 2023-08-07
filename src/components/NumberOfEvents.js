910b786045aaa909da854ac5c632be4424239627
Valeria Balducci <v_balducci@web.de>
2023-08-07 09:05:34 +0200

    try bugfix

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -5,8 +5,19 @@ class NumberOfEvents extends Component {
     number: 32,
   };
 
+  // handleNumberChange = (event) => {
+  //   let inputValue = event.target.value;
+  //   inputValue = Math.max(parseInt(inputValue), 0);
+  //   this.setState({ number: inputValue }, () => {
+  //     this.props.updateEvents(this.props.currentCity, inputValue);
+  //   });
+  // };
+
   handleNumberChange = (event) => {
     let inputValue = event.target.value;
+    if (inputVparseInt(inputValue) < 0) {
+      // show error alert
+    }
     inputValue = Math.max(parseInt(inputValue), 0);
     this.setState({ number: inputValue }, () => {
       this.props.updateEvents(this.props.currentCity, inputValue);
